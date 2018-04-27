c0bf1321cbc4909e913d89738bb111ba04b87343
JohnAsare <johnasare7@gmail.com>
2018-04-27 18:35:07 -0400

    Implemented the Minstack algorithm

--- Min_stack.py
+++ Min_stack.py
@@ -14,16 +14,39 @@ class Stack:
         self.stack_list, self.min_element = [], []
 
     def push(self, item):
-        self.stack_list.appened(item)
+        self.stack_list.append(item)
         if len(self.min_element):
             if item < self.min_element[-1][0]:
-                self.min_element.appened([item, 1])
+                self.min_element.append([item, 1])
 
         elif item == self.min_element[-1][0]:
             self.min_element[-1][1] += 1
 
         else:
-            self.stack_list.appened([item, 1])
+            self.stack_list.append([item, 1])
+
+    def pop(self):
+        smallest_item = self.stack_list.pop()
+        if smallest_item == self.min_element[-1][0]:
+            self.min_element[-1][1] -= 1
+
+            if self.min_element[-1][1] == 0:
+                self.min_element.pop()
+
+    def peek(self):
+        return self.stack_list[-1]
+
+    def getMin(self):
+        return self.min_element[-1][0]
+
+
+if __name__ == "__main__":
+    stack_lyst = Stack()
+    stack_lyst.push(-1)
+
+    print(stack_lyst.top(), stack_lyst.getMin())
+
+
 
 
 
