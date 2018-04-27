7cf2acf13bbced7ba4f638f181b731dd1e9350a1
JohnAsare <johnasare7@gmail.com>
2018-04-27 13:11:44 -0400

    Min staack
    Creating  a new function to return the minimum number is a stack.

---
+++ Min_stack.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env Python3
+
+# Name: John Asare
+# Date: Wed Apr 25 2018 12:39
+
+""" Min Stack: A Stack implementation getting the min in the list """
+
+
+class MinStack:
+
+    def __init__(self):
+        self.lyst = []
+
+    def push(self, x):
+        self.lyst.append(x)
+
+    def pop(self):
+        return self.lyst.pop()
+
+    def top(self):
+        return self.lyst[len(self.lyst - 1)]
+
+
+
