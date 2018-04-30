d5f67b6712163bf3ec5bf81fc7030b2f47705016
JohnAsare <johnasare7@gmail.com>
2018-04-30 00:10:54 -0400

    top is changed to peek and introduced size and empty function

--- Min_stack.py
+++ Min_stack.py
@@ -7,39 +7,40 @@
 
 """ For description check  John Asare on GitHub """
 
-
 class Stack:
+
     def __init__(self):
-        self.stack, self.minStack = [], []
+        self.stack, self.Minstack = [],[]
+
+    def push(self, item):
+        self.stack.append(item)
+        if len(self.Minstack):
+            if item < self.Minstack[-1][0]:
+                self.Minstack.append([item, 1])
+            elif item == self.Minstack[-1][0]:
+                self.Minstack[-1][1] += 1
+        else:
+            self.Minstack.append([item, 1])
+
+    def pop(self):
+        item = self.stack.pop()
+        if item == self.Minstack[-1][0]:
+            self.Minstack[-1][1] += -1
+
+            if self.Minstack[-1][1] == 0:
+                self.Minstack.pop()
+
+    def getMin(self):
+        return self.Minstack[-1][0]
+
+    def peek(self):
+        return self.stack[-1]
 
     def isEmpty(self):
-        return self.stack == []
+        return self.stack == 0
 
     def size(self):
         return len(self.stack)
-
-    def push(self, x):
-        self.stack.append(x)
-        if len(self.minStack):
-            if x < self.minStack[-1][0]:
-                self.minStack.append([x, 1])
-            elif x == self.minStack[-1][0]:
-                self.minStack[-1][1] += 1
-        else:
-            self.minStack.append([x, 1])
-
-    def pop(self):
-        x = self.stack.pop()
-        if x == self.minStack[-1][0]:
-            self.minStack[-1][1] -= 1
-            if self.minStack[-1][1] == 0:
-                self.minStack.pop()
-
-    def top(self):
-        return self.stack[-1]
-
-    def getMin(self):
-        return self.minStack[-1][0]
 
 
 if __name__ == "__main__":
@@ -49,7 +50,7 @@ if __name__ == "__main__":
     stack_lyst.push(23)
     stack_lyst.pop()
 
-    print(stack_lyst.top(), '\n', stack_lyst.getMin(), '\n', stack_lyst.isEmpty(), '\n', stack_lyst.size())
+    print(stack_lyst.peek(), '\n', stack_lyst.getMin(), '\n', stack_lyst.isEmpty(), '\n', stack_lyst.size())
 
 
 
